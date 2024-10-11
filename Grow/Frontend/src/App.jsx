2a7df9325379a7da4d5472f6ee4673451eb36e31
Leonardo Paz <flajarkin99@hotmail.com>
2024-10-11 10:15:27 -0300

    [UPDATE] Estructura de carpetas

---
+++ Grow/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> GROW </h1>
+      </div>
+      <h1>Vite + React</h1>
+      <button type="button" className='btn btn-success'> Boton de Bootstrap </button>
+      
+    </>
+  )
+}
+
+export default App
