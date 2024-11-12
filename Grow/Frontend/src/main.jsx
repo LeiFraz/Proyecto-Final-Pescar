b00f0c4a3d790dadb4267d20a53ff94739272e30
Carlos13X <carlos95_2012@hotmail.com>
2024-11-12 16:51:38 -0300

    eliminacion de rutas main

--- Grow/Frontend/src/main.jsx
+++ Grow/Frontend/src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import CompanyUserProfile from './pages/CompanyUserProfile/CompanyUserProfile.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
-    <Router> {/* guardar aca las Rutas de las paginas para direccionar*/}
-      <Routes>
-        <Route path="/" element={<App />} /> {/* Ruta principal INDEX */}
-        <Route path="/perfil" element={<CompanyUserProfile />} /> {/* Ruta PERFIL USUARIO*/}
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+      <App />
+  </React.StrictMode>
 )
