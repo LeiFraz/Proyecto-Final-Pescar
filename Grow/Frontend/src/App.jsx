baada22488ab64e22b5863bd15a6583b31058161
Carlos13X <carlos95_2012@hotmail.com>
2024-11-12 16:43:57 -0300

    perfil emprendedor galeria card

--- Grow/Frontend/src/App.jsx
+++ Grow/Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Home from './pages/home'
 
+import { Link } from 'react-router-dom'; // Importa Link
+
 function App() {
 
   return (
@@ -11,6 +13,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <button type="button" className='btn btn-success'> Boton de Bootstrap </button>
+      
+      {/* Usando Link para navegar a la página de perfil */}
+      <Link to="/perfil">
+        <button type="button" className="btn btn-success"> Botón de Usuario Perfil </button>
+      </Link>
+      
       
     </>
   )
--- Grow/Frontend/src/main.jsx
+++ Grow/Frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import CompanyUserProfile from './pages/CompanyUserProfile/CompanyUserProfile.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Router> {/* guardar aca las Rutas de las paginas para direccionar*/}
+      <Routes>
+        <Route path="/" element={<App />} /> {/* Ruta principal INDEX */}
+        <Route path="/perfil" element={<CompanyUserProfile />} /> {/* Ruta PERFIL USUARIO*/}
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
