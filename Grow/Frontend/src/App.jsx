05208f75d84bceefe9bf764a70ce6468abbec12f
Leonardo Paz <flajarkin99@hotmail.com>
2024-11-12 17:10:08 -0300

    [FIX] App.js

--- Grow/Frontend/src/App.jsx
+++ Grow/Frontend/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes } from './routes/Routes'
 
-import { Link } from 'react-router-dom'; // Importa Link
-
 function App() {
 
   return (
@@ -11,12 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes/>
       </BrowserRouter>
-      
-      {/* Usando Link para navegar a la página de perfil */}
-      <Link to="/perfil">
-        <button type="button" className="btn btn-success"> Botón de Usuario Perfil </button>
-      </Link>
-      
       
     </>
   )
