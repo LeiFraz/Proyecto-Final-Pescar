c7fe3d1b21f4cf4a3ed5a75470b8c3dcc3908cc5
Federico Zapata <federico11zapata@gmail.com>
2024-12-11 19:03:37 -0300

    add carrusel con swiper

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import CategoryCard from "../../components/CategoryCard/CategoryCard";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
+
 import 'swiper/swiper-bundle.min.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
@@ -137,33 +138,55 @@ function Index() {
                         <h2 className="section-subtitle">Productos Recomendados</h2><a href="/publicaciones?tipo=producto">Ver más <i className="icon-right"></i></a>
                     </div>
                     <div className="products-container">
+                        {/* ----------SWIPER-------------------- */}
+                    <Swiper
+                            spaceBetween={10}  // Espacio entre los slides
+                            slidesPerView={1}  // Siempre muestra un slide por vista
+                            loop={true}         // Hace que el swiper sea cíclico, puedes quitarlo si no lo necesitas
+                            breakpoints={{
+                              320: {
+                                slidesPerView: 1,  // En pantallas pequeñas solo muestra un slide
+                              },
+                              768: {
+                                slidesPerView: 2,  // Muestra 2 slides cuando la pantalla es más grande
+                              },
+                              1024: {
+                                slidesPerView: 3,  // Muestra 3 slides cuando la pantalla es aún más grande
+                                },
+                            }}
+                        >
                         {/* <!-- Cards products --> */}
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://i.pinimg.com/originals/1a/e7/5d/1ae75d4027c349a8a9d56714370612cf.jpg"
                         productName="Peluches a Crochet Personalizados"
                         profileName="Crochetizate"
                         originalPrice={30000}
                         discount={20}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://d22fxaf9t8d39k.cloudfront.net/d77b447786f2bdd7994fddc3bfd4e52d190a7c608926e912dc7d16a96500ab1d14657.png"
                         productName="Mazo de Cartas Personalizado"
                         profileName="CarteandoAndo"
                         originalPrice={25000}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://fbi.cults3d.com/uploaders/14619068/illustration-file/84bee593-e0c1-4a9d-ab3d-4e4846df1a12/017.jpg"
                         productName="Figura impresa 3D a elección"
                         profileName="Impresiones Xtreme"
                         originalPrice={0}
                         discount={0}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://i.pinimg.com/736x/f0/6c/88/f06c88e5b39d2c878be2427db5d09a1c.jpg"
                         productName="Torta personalizada"
                         profileName="Beli Pasteleria"
                         originalPrice={35500}
-                        />
+                        /></SwiperSlide>
+                        </Swiper>
                     </div>
                 </section>
             </div>
@@ -175,32 +198,55 @@ function Index() {
                     </div>
                     <div className="products-container">
                         {/* <!-- Cards services --> */}
+                        {/* -------------SWIPER------------------------ */}
+                        <Swiper
+                            spaceBetween={10}  // Espacio entre los slides
+                            slidesPerView={1}  // Siempre muestra un slide por vista
+                            loop={true}         // Hace que el swiper sea cíclico, puedes quitarlo si no lo necesitas
+                            breakpoints={{
+                              320: {
+                                slidesPerView: 1,  // En pantallas pequeñas solo muestra un slide
+                              },
+                              768: {
+                                slidesPerView: 2,  // Muestra 2 slides cuando la pantalla es más grande
+                              },
+                              1024: {
+                                slidesPerView: 3,  // Muestra 3 slides cuando la pantalla es aún más grande
+                                },
+                            }}
+                        >
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://institutotecnicorivadavia.com/webfiles/rivadavia/productos/68/2_1000x1000.jpg"
                         productName="Instalación de aire acondicionado"
                         profileName="Juan Instalaciones"
                         originalPrice={70000}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://www.spaoneandonly.cl/wp-content/uploads/2024/09/C7AD8ED4-BA67-4775-BA6A-BFC6832F6BC7-1-1000x1000.jpg"
                         productName="Sesión de masaje por hora"
                         profileName="MAXajes"
                         originalPrice={10000}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://siberiasalon.com/wp-content/smush-webp/2023/09/Glitter-1000x1000.jpg.webp"
                         productName="Uñas permanentes personalizadas"
                         profileName="Marlu Nails"
                         originalPrice={8000}
                         discount={25}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://grupolasser.com/wp-content/uploads/2022/07/empresa-instalacion-camaras-empresas-comunidades-hogares-madrid.jpg"
                         productName="Instalación de camaras de seguridad"
                         profileName="SecurityCam"
                         originalPrice={110000}
                         discount={10}
-                        />
+                        /></SwiperSlide>
+
+                    </Swiper>
                     </div>
                 </section>
             </div>
@@ -211,65 +257,108 @@ function Index() {
                         <h2 className="section-subtitle">Ofertas Disponibles</h2><a href="/publicaciones?descuento=5&orden=MasDescuento">Ver más <i className="icon-right"></i></a>
                     </div>
                     <div className="products-container">
+                    <Swiper
+                            spaceBetween={10}  // Espacio entre los slides
+                            slidesPerView={1}  // Siempre muestra un slide por vista
+                            loop={true}         // Hace que el swiper sea cíclico, puedes quitarlo si no lo necesitas
+                            breakpoints={{
+                              320: {
+                                slidesPerView: 1,  // En pantallas pequeñas solo muestra un slide
+                              },
+                              768: {
+                                slidesPerView: 2,  // Muestra 2 slides cuando la pantalla es más grande
+                              },
+                              1024: {
+                                slidesPerView: 3,  // Muestra 3 slides cuando la pantalla es aún más grande
+                                },
+                            }}
+                        >
                         {/* <!-- Cards services products discount --> */}
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="http://www.mesasdepool.com.ar/images/productos/Directorio/Directorio-01.jpg"
                         productName="Mesa de pool giratoria"
                         profileName="Piramide Pool"
                         originalPrice={1000000}
                         discount={70}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi8vNpeOn4mAIBd8SIfu7BqHUlmD-Qcj0Pmw&s"
                         productName="Reparación de PC"
                         profileName="Surfear"
                         originalPrice={30000}
                         discount={60}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl=""
                         productName="Soldador profesional a domicilio"
                         profileName="SPD Metales"
                         originalPrice={60000}
                         discount={30}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProductCard
                         imageUrl="https://acdn.mitiendanube.com/stores/002/897/767/products/balde-de-albanil-comun11-5b35b60800072eacfb16882243036520-1024-1024.png"
                         productName="Albañileria por dia"
                         profileName="Raul Paez"
                         originalPrice={35514}
                         discount={20}
-                        />
+                        /></SwiperSlide>
+                        </Swiper>
                     </div>
                 </section>
             </div>
+
             <div className="main-container">
                 <section className="entrepreneurships-section" id="entrepreneurships-section">
                     <div className="section-title">
                         <h2 className="section-subtitle">Emprendimientos Recomendados</h2><a href="/emprendimientos">Ver más <i className="icon-right"></i></a>
                     </div>
                     <div className="profiles-container">
+                    <Swiper
+                            spaceBetween={10}  // Espacio entre los slides
+                            slidesPerView={1}  // Siempre muestra un slide por vista
+                            loop={true}         // Hace que el swiper sea cíclico, puedes quitarlo si no lo necesitas
+                            breakpoints={{
+                              320: {
+                                slidesPerView: 1,  // En pantallas pequeñas solo muestra un slide
+                              },
+                              768: {
+                                slidesPerView: 2,  // Muestra 2 slides cuando la pantalla es más grande
+                              },
+                              1024: {
+                                slidesPerView: 3,  // Muestra 3 slides cuando la pantalla es aún más grande
+                                },
+                            }}
+                        >
                         {/* <!-- Cards profiles --> */}
+                        <SwiperSlide>
                         <ProfileCard
                         imageUrl={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJQReQFArnzkZPKmQ-YbY0EPY8TObhOlbSTQ&s"}
                         profileName={"Piramide Pool"}
                         description={"Pirámide es una empresa familiar que hace 25 años se dedica a la fabricación de diseños exclusivos de juegos de salón. Contamos con una amplia variedad de diseños, que se adaptan al gusto y necesidad de cada cliente. En la web se encuentran todos los modelos disponibles y, para una atención más personalizada, tenemos abierto nuestro showroom, atendido por sus dueños, quienes les brindarán una excelente atención."}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProfileCard
                         imageUrl={"https://i.postimg.cc/hPBVg96b/logooooooooo.png"}
                         profileName={"Beli Pasteleria"}
                         description={"Pasteleria artesanal 🍪 📍José C Paz | San Miguel | Bella vista (Consulte por otras zonas)"}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProfileCard
                         imageUrl={"https://marketplace.canva.com/EAE_Xy5fMq0/1/0/1600w/canva-logo-floral-para-emprendimiento-accesorios-jxJv6i75-lc.jpg"}
                         profileName={"Alta Pinta Accesorios"}
                         description={"Accesorios de todo tipo, llaveros, pines, pulseras, collares, todo hecho al detalle."}
-                        />
+                        /></SwiperSlide>
+                        <SwiperSlide>
                         <ProfileCard
                         imageUrl={"https://creadordelogos.com.ar/_next/image/?url=%2Fimages%2Fes-AR%2Flogos%2Faurora.png&w=1920&q=75"}
                         profileName={"Aurora"}
                         description={"Productos para tu mascota, alimento balanceado, camas, cuchas, rascadores y juguetes para tu amigo peludo."}
-                        />
+                        /></SwiperSlide>
+                        </Swiper>
                     </div>
                 </section>
             </div>
